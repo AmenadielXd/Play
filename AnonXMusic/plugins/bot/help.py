4e7e8b1f59c92d14f37d808c30d866524efde18f
AmenadielXd <rv1858000@gmail.com>
2025-01-17 14:35:48 +0530

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -1,3 +1,4 @@
+"""
 from typing import Union
 
 from pyrogram import filters, types
@@ -88,3 +89,4 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
     elif cb == "hb15":
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
+"""
