ba638bf53d6b9147fdb9d504c931cb616e131678
AmenadielXd <rv1858000@gmail.com>
2025-01-18 00:17:19 +0530

    Update blchat.py

--- AnonXMusic/plugins/sudo/blchat.py
+++ AnonXMusic/plugins/sudo/blchat.py
@@ -1,3 +1,4 @@
+"""
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -59,3 +60,4 @@ async def all_chats(client, message: Message, _):
         await message.reply_text(_["black_8"].format(app.mention))
     else:
         await message.reply_text(text)
+"""
