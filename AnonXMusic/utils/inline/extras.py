f6f33975680153df33f65c36fe6c4891d6c224b2
AmenadielXd <rv1858000@gmail.com>
2025-01-17 10:22:37 +0530

    Update extras.py

--- AnonXMusic/utils/inline/extras.py
+++ AnonXMusic/utils/inline/extras.py
@@ -6,7 +6,9 @@ from config import SUPPORT_CHAT
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
+            InlineKeyboardButton(text="𝑆𝑒𝑐𝑟𝑒𝑡 𝐼𝑛𝑙𝑖𝑛𝑒", switch_inline_query_current_chat="")
+        ],
+        [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
