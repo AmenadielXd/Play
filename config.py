c9655bea184c050c43b8691467c936eb880d10fc
AmenadielXd <rv1858000@gmail.com>
2025-01-18 21:28:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,7 +83,8 @@ def time_to_seconds(time):
 
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
-
+SONG_DOWNLOAD_DURATION_LIMIT = int(
+    time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00"))
 
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
