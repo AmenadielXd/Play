ff5d1eb54c36a8f74b9d5cfc0536a35a216082bd
AmenadielXd <rv1858000@gmail.com>
2025-01-18 09:45:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,64 +6,35 @@ from pyrogram import filters
 
 load_dotenv()
 
-# Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
-# Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
-
-# Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-
-# Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-
-# Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6502950481))
 BOT_NAME = getenv("BOT_NAME", "Nova UI")
 BOT_USERNAME = getenv("BOT_USERNAME", "novauibot")
-
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/AmenadielXd/AlexPlay",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
-
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/none")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/none")
 
-# Set this to True if you want the assistant to automatically leave chats after an interval
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AmenadielXd/AlexPlay",)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
+
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 3000))
 
-
-# Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
-
-# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
-
-# Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
-# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
-# Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
@@ -79,18 +50,6 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://unitedcamps.in/Images/file_5843.jpg"
-["https://unitedcamps.in/Images/file_10524.jpg",
-"https://unitedcamps.in/Images/file_10523.jpg",
-"https://unitedcamps.in/Images/file_10522.jpg",
-"https://unitedcamps.in/Images/file_10521.jpg",
-"https://unitedcamps.in/Images/file_10520.jpg",
-"https://unitedcamps.in/Images/file_10519.jpg",
-"https://unitedcamps.in/Images/file_10518.jpg",
-"https://unitedcamps.in/Images/file_10517.jpg",
-"https://unitedcamps.in/Images/file_10516.jpg",
-"https://unitedcamps.in/Images/file_10515.jpg",
-"https://unitedcamps.in/Images/file_10514.jpg", ]
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
