50cba6a222e5f573479a1d872d72148e02133a75
AmenadielXd <rv1858000@gmail.com>
2025-01-18 08:16:36 +0530

    Update and rename shuffle.py to shuffle.py

--- Play/plugins/admins/shuffle.py
+++ Play/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Play import app
+from Play.misc import db
+from Play.utils.decorators import AdminRightsCheck
+from Play.utils.inline import close_markup
 from config import BANNED_USERS
 
 
