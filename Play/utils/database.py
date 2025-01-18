cd450f84f4e460e320d0df46e856d7b2fc6cec70
AmenadielXd <rv1858000@gmail.com>
2025-01-18 07:47:36 +0530

    Update and rename database.py to database.py

--- Play/utils/database.py
+++ Play/utils/database.py
@@ -1,25 +1,21 @@
 import random
 from typing import Dict, List, Union
 
-from AnonXMusic import userbot
-from AnonXMusic.core.mongo import mongodb
+from Play import userbot
+from Play.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
 autoenddb = mongodb.autoend
 assdb = mongodb.assistants
-blacklist_chatdb = mongodb.blacklistChat
-blockeddb = mongodb.blockedusers
 chatsdb = mongodb.chats
 channeldb = mongodb.cplaymode
 countdb = mongodb.upcount
-gbansdb = mongodb.gban
 langdb = mongodb.language
 onoffdb = mongodb.onoffper
 playmodedb = mongodb.playmode
 playtypedb = mongodb.playtypedb
 skipdb = mongodb.skipmode
-sudoersdb = mongodb.sudoers
 usersdb = mongodb.tgusersdb
 
 # Shifting to memory [mongo sucks often]
@@ -67,7 +63,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from Play.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -81,7 +77,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AnonXMusic.core.userbot import assistants
+    from Play.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -108,7 +104,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from Play.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -121,7 +117,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AnonXMusic.core.userbot import assistants
+    from Play.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
