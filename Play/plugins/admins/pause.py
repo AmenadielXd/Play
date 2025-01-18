25813eeb2b51eb2b98b49c0017037aaab247aff6
AmenadielXd <rv1858000@gmail.com>
2025-01-18 08:08:59 +0530

    Update and rename pause.py to pause.py

--- Play/plugins/admins/pause.py
+++ Play/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Play import app
+from Play.core.call import Anony
+from Play.utils.database import is_music_playing, music_off
+from Play.utils.decorators import AdminRightsCheck
+from Play.utils.inline import close_markup
 from config import BANNED_USERS
 
 
