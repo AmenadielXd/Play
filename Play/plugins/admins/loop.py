d4c6f5de1014bbd0eba1ff767c858ef22d9105a1
AmenadielXd <rv1858000@gmail.com>
2025-01-18 08:07:04 +0530

    Update and rename loop.py to loop.py

--- Play/plugins/admins/loop.py
+++ Play/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Play import app
+from Play.utils.database import get_loop, set_loop
+from Play.utils.decorators import AdminRightsCheck
+from Play.utils.inline import close_markup
 from config import BANNED_USERS
 
 
