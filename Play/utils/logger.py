75f8f55e8b116a305b889d421a0c225104ad0855
AmenadielXd <rv1858000@gmail.com>
2025-01-18 07:35:32 +0530

    Update and rename logger.py to logger.py

--- Play/utils/logger.py
+++ Play/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import is_on_off
+from Play import app
+from Play.utils.database import is_on_off
 from config import LOGGER_ID
 
 
